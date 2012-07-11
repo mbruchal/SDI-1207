f194f27bdbe12915c29f698733bc16fae3bc4c45
Michael Bruchal <sashalyn27@Sherilyn-Bruchals-iMac.local>
2012-07-10 16:21:58 -1000

    7th update to project2

--- Bruchal_Michael_assignment2.js
+++ Bruchal_Michael_assignment2.js
@@ -20,46 +20,40 @@ var startRoutine = function (gymIsOpen) {
 	 console.log("Oh well! Lets get more tattoos instead.");
   }
 };
-startRoutine (gymIsOpen);
+
 
 //Boolean Function
-var weightRoom = function (liftWeights, runAllDay) {
+var weightRoom = function (gymIsOpen, weightRoomIsOpen) {
 	  if ((gymIsOpen && weightRoomIsOpen) || cardioRoomIsOpen) {
     console.log(me + " and " + brother + " is gonna hit the weights!");
   } else {
     console.log("I guess we can run on the road.");
-}
+  }
 	 itsOn = (gymIsOpen && weightRoomIsOpen);
 	 return itsOn;
 };
-weightRoom(brother, me);
-	  console.log("Lets do chest. " + me + " is first!");
+
 
 //Number Function
-var startingWeight = function () {
-	  var startIbs = 205;
+var startingWeight = function (startIbs) {
 	  while (startIbs <= 315) {
 		console.log(startIbs + " pounds is too light!");
 		startIbs+=25;}
-		console.log(305 + " pounds is the maximum. Its " + brother + "'s turn.");
-		return startIbs;
+		console.log(305 + " pounds is the maximum.");
+	 return startIbs;
 }; 
-startingWeight();
-console.log("Lets take a break, gotta rehydrate!");
+
 
 //String Function
 var energyDrink = function (time, name, message) {
 	 var drink1 = "redbull";
-	 var timeLeft = 0
+	 var timeLeft = 0;
 	 console.log(time + " minutes left" + name + message + " finish his " + drink1 + ".");
 	 if (timeLeft < 5) {
-	 console.log("Your done. Next Exercise!");
-	 return timeLeft;
-		
+	 console.log(name + " is done. Next Exercise!");
 	}
+	 return timeLeft;
 };	
-energyDrink(5, " Mike", " needs to");
-energyDrink(5, " Jon", " needs to");
 
 //Array Function
 
@@ -67,17 +61,31 @@ energyDrink(5, " Jon", " needs to");
 var firstExercise = function (burnIt) {
 	var exercise = exerciseRoutine[burnIt],
 			 minutesThisExercise = minutesPerExercise[burnIt];
-			console.log("Lets do the " + exercise + " for " + minutesThisExercise + " minutes.");
+			console.log("Lets do the " + exercise + " exercise for " + minutesThisExercise + " minutes.");
 			for (var minutes = 0; minutes < minutesThisExercise; minutes +=5) {
 			 var minutesRemain = minutesThisExercise - minutes;
-			console.log(minutes + " minutes done, " + minutesRemain + " minutes to go!");
-			}
-			console.log("We have reached muscle failure!");
+			 console.log(minutes + " minutes done, " + minutesRemain + " minutes to go!");
+			 }
+			 console.log("We have reached muscle failure!");
+	return exercise;
 };
-var finish = function () {
+var finish = function (burnIt) {
 	for (var burnIt = 0; burnIt < exerciseRoutine.length; burnIt++) {
 			firstExercise (burnIt);}
 };
+startRoutine (gymIsOpen);
+
+weightRoom(true, true);
+console.log("Lets do chest. " + me + " is first!");
+
+startingWeight(205);
+console.log ("Its " + brother + "'s turn");
+
+startingWeight(205);
+
+energyDrink(5, " Mike", " needs to");
+energyDrink(5, " Jon", " needs to");
+
 finish();
 console.log("That was a good workout. Now lets get something to eat.");
 
