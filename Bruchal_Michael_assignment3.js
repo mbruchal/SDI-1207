41011f60c86c3c905e0d4d34fccc9fa482657f15
Michael Bruchal <sashalyn27@Sherilyn-Bruchals-iMac.local>
2012-07-11 22:13:55 -1000

    1st update to project3

--- Bruchal_Michael_assignment3.js
+++ Bruchal_Michael_assignment3.js
@@ -3,3 +3,24 @@
    Deliverable 3
 */
 
+//Global Variables
+var destination1 = "Sacramento",
+    destination2 = "San Francisco",
+    destination3 = "Berkley";
+
+//Properties
+var me = {
+          name: "Mike",		//string
+          male: true,       //boolean
+          age: 32,          //number
+          height: "6'0",
+          weight: 195,
+          status: ["Employed", "Student", "Married"],    //array
+          records: [									 //object
+                    {bench: 315,
+                     shoulderPress: 225,
+                     squat: 365,
+                     deadlift: 405,
+                    }
+                   ]
+};
