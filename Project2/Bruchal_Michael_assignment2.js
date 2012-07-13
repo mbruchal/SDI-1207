dcd06b3dc4a3f1d0546ccf6cdc74d5779da9ddf3
Michael Bruchal <sashalyn27@Sherilyn-Bruchals-iMac.local>
2012-07-12 17:32:03 -1000

    another update to project 2

--- Project2/Bruchal_Michael_assignment2.js
+++ Project2/Bruchal_Michael_assignment2.js
@@ -16,21 +16,21 @@ var gym = "Travis AFB Fitness Center",
 var startRoutine = function (gymIsOpen) {
     if (gymIsOpen === true) {
 	 console.log("Lets go to " + gym + " and get started!");
-  } else {
-	 console.log("Oh well! Lets get more tattoos instead.");
-  }
+     } else {
+	         console.log("Oh well! Lets get more tattoos instead.");
+       }
 };
 
 
 //Boolean Function
 var weightRoom = function (gymIsOpen, weightRoomIsOpen) {
-	  if ((gymIsOpen && weightRoomIsOpen) || cardioRoomIsOpen) {
-    console.log(me + " and " + brother + " is gonna hit the weights!");
-  } else {
-    console.log("I guess we can run on the road.");
-  }
-	 itsOn = (gymIsOpen && weightRoomIsOpen);
-	 return itsOn;
+	 if ((gymIsOpen && weightRoomIsOpen) || cardioRoomIsOpen) {
+      console.log(me + " and " + brother + " is gonna hit the weights!");
+     } else {
+        console.log("I guess we can run on the road.");
+       }
+	    itsOn = (gymIsOpen && weightRoomIsOpen);
+return itsOn;
 };
 
 
@@ -38,9 +38,10 @@ var weightRoom = function (gymIsOpen, weightRoomIsOpen) {
 var startingWeight = function (startIbs) {
 	  while (startIbs <= 315) {
 		console.log(startIbs + " pounds is too light!");
-		startIbs+=25;}
-		console.log(305 + " pounds is the maximum.");
-	 return startIbs;
+		startIbs+=25;
+	  }
+		var max = 305 + " pounds is the maximum.";
+return max;
 }; 
 
 
@@ -49,10 +50,10 @@ var energyDrink = function (time, name, message) {
 	 var drink1 = "redbull";
 	 var timeLeft = 0;
 	 console.log(time + " minutes left" + name + message + " finish his " + drink1 + ".");
-	 if (timeLeft < 5) {
-	 console.log(name + " is done. Next Exercise!");
-	}
-	 return timeLeft;
+	     if (timeLeft < 5) {
+	     console.log(name + " is done. Next Exercise!");
+	     }
+return timeLeft;
 };	
 
 //Array Function
@@ -61,31 +62,41 @@ var energyDrink = function (time, name, message) {
 var firstExercise = function (burnIt) {
 	var exercise = exerciseRoutine[burnIt],
 			 minutesThisExercise = minutesPerExercise[burnIt];
-			console.log("Lets do the " + exercise + " exercise for " + minutesThisExercise + " minutes.");
-			for (var minutes = 0; minutes < minutesThisExercise; minutes +=5) {
-			 var minutesRemain = minutesThisExercise - minutes;
-			 console.log(minutes + " minutes done, " + minutesRemain + " minutes to go!");
-			 }
-			 console.log("We have reached muscle failure!");
-	return exercise;
+			 console.log("Lets do the " + exercise + " exercise for " + minutesThisExercise + " minutes.");
+		for (var minutes = 0; minutes < minutesThisExercise; minutes +=5) {
+		     var minutesRemain = minutesThisExercise - minutes;
+		     console.log(minutes + " minutes done, " + minutesRemain + " minutes to go!");
+		}
+             
 };
-var finish = function (burnIt) {
-	for (var burnIt = 0; burnIt < exerciseRoutine.length; burnIt++) {
-			firstExercise (burnIt);}
+
+var finish = function () {
+	    for (var burnIt = 0; burnIt < exerciseRoutine.length; burnIt++) {
+			firstExercise (burnIt);
+		}
+             var startEx = me + " and " + brother + " have reached muscle failure.";
+return startEx;
 };
+
 startRoutine (gymIsOpen);
 
 weightRoom(true, true);
 console.log("Lets do chest. " + me + " is first!");
 
+var begin = startingWeight(205);	//fixed return value here
+console.log(begin);
+console.log("Its " + brother + "'s turn");
+ 
 startingWeight(205);
-console.log ("Its " + brother + "'s turn");
+console.log(begin);
 
-startingWeight(205);
+console.log("Gots to rehydrate lets take a break.");
 
 energyDrink(5, " Mike", " needs to");
 energyDrink(5, " Jon", " needs to");
 
-finish();
+var endEx = finish();	//fixed return value here
+console.log(endEx);
 console.log("That was a good workout. Now lets get something to eat.");
+
 
