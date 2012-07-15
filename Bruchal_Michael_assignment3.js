c0966d4505f10f00662d3c32c2b72ee11207b36f
Michael Bruchal <sashalyn27@Sherilyn-Bruchals-iMac.local>
2012-07-15 04:04:14 -1000

    5th update to project3

--- Bruchal_Michael_assignment3.js
+++ Bruchal_Michael_assignment3.js
@@ -11,11 +11,11 @@ var destination1 = "Sacramento",
 //Properties
 var me = {
           name: "Mike",   //string
-          male: true,       //boolean
+          motivated: true,       //boolean
           age: 32,          //number
           height: "6'0",
           weight: 195,
-          goal: ["Bench: " + 365 + " Shoulderpress: " + 275 + " Squat: " + 465 + " DeadLift: " + 520],   //array
+          goal: ["Bench: " + 365 + ", Shoulderpress: " + 275 + ", Squat: " + 465 + ", DeadLift: " + 520],   //array
           records: [                   //object
                     {
                      bench: "315 Ibs.",
@@ -32,6 +32,14 @@ var me = {
                      console.log ("I qualify for the Middle Weight Division.");
                   };    
                 
+          },
+          mentalPrep: function (willing, tired) {   //method function, Argument Boolean
+              if ((me.motivated && willing) || tired) {
+                console.log ("Time to start eating clean.");
+              } else {
+                  console.log ("I'll just goof around in " + destination1 + ", " + destination2 + ", and " + destination3 + " instead.");
+              };
+              return true;    //return Boolean
           },
           getTickets: function (wallet) {     //Method Accessor, Argument Number
                 var compOne = 75,
@@ -58,13 +66,27 @@ var me = {
                   result = heavy + " is perfect. " + light + " was too light.";
                   return result;    //return string
           },
-          changeGoal: function (newGoal) {     //Mutator Function, Array Argument
-              var newGoal = ["Bench: " + 380 + ", Shoulderpress: " + 295 + ", Squat: " + 405 + ", Deadlift: " + 405];
+          changeGoal: function (newGoal) {     //Mutator Function, Argument Array
+              var newGoal = ["Bench: " + 380 + ", Shoulderpress: " + 295 + ", Squat: " + 405 + ", Deadlift: " + 405 + "."];
               me.goal = newGoal
               return newGoal    //Return Array
           },
-          startPlan: function () {
-              console.log (exRoutine.exercisePlan["week1"]);
+          startPlan: function () {    //Method Function, json data, return object
+              layout = exRoutine.exercisePlan[0];
+                return layout;
+          },
+          schedule: function () {     //Method Function, 
+            var weeks = 4,
+                off = 0;
+              for (var i = 0; i < weeks; i++) {   //For Loop
+                var weeksRemain = weeks - i;
+                  console.log (i + " weeks down. " + weeksRemain + " weeks to go.");
+                    for (var sessions = 4; off < sessions; sessions--) {    //Nested Loop
+                      var sessionsRemain = sessions - off;
+                      console.log (sessionsRemain + " more trips to the gym.");
+                      
+                    };
+              };
           }
 
 };
@@ -74,17 +96,17 @@ me.train();
 console.log ("Three different shows to choose from in " + destination1 + ", " + destination2 + ", and " + destination3 + ".");
 var balance = me.getTickets(300);
 console.log (balance);
+me.mentalPrep(true, false);
 console.log ("Got to reach my lift goal first.");
 console.log ("My current records are....");
 console.log (me.records);
-console.log ("I am currently " + me.weight + " pounds and neet to be at 210.")
+console.log ("I am currently " + me.weight + " pounds and need to be at 210.")
 var weightWatch = me.weightGoal("210 Ibs.", "190 Ibs.");
 console.log (weightWatch);
-console.log ("My 1st goal plan - " + me.goal + ". Decided to change it up");
+console.log ("My 1st goal plan - " + me.goal + ".");
 var revised = me.changeGoal();
 console.log ("Change of plans to new goal - " + revised);
-me.startPlan();
-
-
-
+me.schedule();
+var displayPlan = me.startPlan();
+console.log (displayPlan);
 
