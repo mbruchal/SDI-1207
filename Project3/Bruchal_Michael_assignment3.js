3aeae5f6ab6abbb125e1770a6074744116765d07
Michael Bruchal <sashalyn27@Sherilyn-Bruchals-iMac.local>
2012-07-17 20:45:21 -1000

    10th update to project3

--- Project3/Bruchal_Michael_assignment3.js
+++ Project3/Bruchal_Michael_assignment3.js
@@ -24,11 +24,14 @@ var me = {
                      deadlift: "405 Ibs.",
                     }
                    ],
+          getAge: function () {     //Accessor Method
+            return me.weight;       //Return Number
+          },
           getName: function () {    //Accessor Method
-              return me["name"];      //Return String
+            return me["name"];      //Return String
           },
           train: function (workOutType) {     //Method Procedure, Argument Object
-              console.log ("This is my 2nd BodyBuilding Competition in Northern California.");
+              console.log ("This is my 2nd Arnold classic BodyBuilding Competition in Northern California.");
                 if (workOutType === exRoutine.exercisePlan[0]) {        //Conditional/json data
                   console.log ("I qualify for the Light Heavy Weight divison.");
                 } else {
@@ -66,24 +69,24 @@ var me = {
                 console.log ("Eating more protein to make weight. " + me.weight + " pounds and counting....");
                   me.weight +=2;
               }; 
-                  result = heavy + " is perfect. " + light + " was too light.";
+                  result = heavy + " is perfect. " + light + " was way too light.";
                     return result;    //return string
           },
           changeGoal: function (newGoal) {     //Mutator Function, Argument Array
               me.goal = newGoal
                 return newGoal    //Return Array
           },
-          startPlan: function () {    //Method Function,  
+          startPlan: function () {    //Method Function  
               layout = exRoutine.exercisePlan[0];   //json data
-                return layout;    //return object
+                return layout;    //Return Object
           },
-          schedule: function () {     //Method Function, 
-              var weeks = 4,
+          schedule: function () {     //Method Function 
+              var weeks = 4,          //Local Variables
                   off = 0;
               for (var i = 0; i < weeks; i++) {   //For Loop
                 var weeksRemain = weeks - i;
                 console.log (i + " weeks down. " + weeksRemain + " weeks to go.");
-                  for (var sessions = 4; off < sessions; sessions--) {    //Nested Loop
+                  for (var sessions = 4; off < sessions; sessions--) {         //Nested Loop
                     var sessionsRemain = sessions - off;
                     console.log (sessionsRemain + " more trips to the gym.");
                   };
@@ -94,8 +97,9 @@ var me = {
 
 //Output
 console.log ("Hi my name is " + me.getName() + " and this is my Body Building story.");
+console.log ("Most people lose weight to compete, but like arnold did, I need to gain weight. I weigh only " + me.getAge() + " pounds and need to be at 210.");
 me.train(exRoutine.exercisePlan[0]);
-console.log ("Three different shows to choose from in " + destination1 + ", " + destination2 + ", and " + destination3 + ".");
+console.log ("There's other competitions in " + destination1 + ", " + destination2 + ", and " + destination3 + ".");
 var balance = me.signUps(300);
 console.log (balance);
 me.mentalPrep(true, false);
@@ -103,7 +107,7 @@ console.log ("Got to reach my lift goal first.");
 console.log ("My current records are....");
 console.log (me.records);
 console.log ("I am currently " + me.weight + " pounds and need to be at 210.");
-var weightWatch = me.weightGoal("210 Ibs.", "190 Ibs.");
+var weightWatch = me.weightGoal("210 Ibs.", "195 Ibs.");
 console.log (weightWatch);
 console.log ("My 1st goal plan - " + me.goal + ".");
 console.log ("Change of plans to new goal - " + me.changeGoal(["Bench: " + 380 + ", Shoulderpress: " + 295 + ", Squat: " + 405 + ", Deadlift: " + 405 + "."]));
